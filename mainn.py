a10e9e44507231795146fcc457597365785d875e
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2024-02-20 19:54:58 +0200

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -1,4 +1,15 @@
 from utils import is_prime
+from utils import is_power_of_five
+
+def main():
+    number = int(input("Введіть число: "))
+    if is_power_of_five(number):
+        print(f"{number} є степенем числа 5.")
+    else:
+        print(f"{number} не є степенем числа 5.")
+
+if __name__ == "__main__":
+    main()
 
 number = 7
 if is_prime(number):
