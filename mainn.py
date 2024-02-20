cc0942cae888323244bf2e5655c74d7296b6d991
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2024-02-20 19:51:59 +0200

    main and utils

---
+++ mainn.py
@@ -0,0 +1,7 @@
+from utils import is_prime
+
+number = 7
+if is_prime(number):
+    print(f'{number} is a prime number')
+else:
+    print(f'{number} is not a prime number')
---
+++ utils.py
@@ -0,0 +1,15 @@
+def fac(n):
+    factorial = 1
+    for i in range(1, n + 1):
+        factorial *= i
+
+    return factorial
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
